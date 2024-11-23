7a8404abe1b7f4ea017f26423c1b7c5ab6a2ee68
xegaca <jesus_gc_93@hotmail.com>
2024-11-23 20:01:01 +0100

    Añado las funciones de resta, multiplicación y división

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,19 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No es posible dividir por 0"
+    return a / b
+
 if __name__ == "__main__":
     print("Suma:", sumar(5, 3))
+    print("Resta:", restar(5, 3))
+    print("Multiplicación:", multiplicar(5, 3))
+    print("División:", dividir(5, 3))
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,12 +1,26 @@
 import unittest
-from calculadora import sumar
+from calculadora import sumar, restar, multiplicar, dividir
 
 class TestCalculadora(unittest.TestCase):
-
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 3), 2)
+        self.assertEqual(dividir(-6, 3), -2)
+        self.assertEqual(dividir(6, 0), "Error: No es posible dividir por 0")
 
 if __name__ == '__main__':
     unittest.main()
